ab69071e8d85cd7b961a44585214ba6fc2a04211
Billy Hodes <billyhodes@gmail.com>
2018-09-12 22:56:23 -0500

    last commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,137 @@
+var panel = $('#quiz-area');
+
+$(document).on('click', '#start', function() {
+  game.start();
+});
+
+$(document).on('click', '#done', function() {
+  game.done();
+});
+
+
+var questions = [{
+  question: "1 . Who was the first President of the United States?",
+  answers: ["George Washington", "Donald Trump","Abraham Lincoln","John Adams"],
+  correctAnswer: "George Washington"
+}, {
+  question: "2 . What year did the American Revolution end?",
+  answers: ["1776", "1800", "1783", "1775"],
+  correctAnswer: "1783"
+}, {
+  question: "3 . How many amendments to the Constitution are there?",
+  answers: ["27", "30", "10", "None"],
+  correctAnswer: "27"
+}, {
+  question: "4 . Out of the 50 states, which was the last to be admitted?",
+  answers: ["New Mexico", "Alaska", "Hawaii", "Arizona"],
+  correctAnswer: "Hawaii"
+}, {
+  question: "5 . What is the minimum age requirement for the President of the United States?",
+  answers: ["21", "40", "18", "35"],
+  correctAnswer: "35"
+}
+];
+
+var game = {
+  correct:0,
+  incorrect:0,
+  counter:60,
+  countdown: function(){
+    game.counter--;
+    $('#counter-number').html(game.counter);
+
+    if (game.counter === 0){
+      console.log('TIME UP');
+      game.done();
+    }
+  },
+  start: function() {
+    timer = setInterval(game.countdown, 1000);
+
+    $('#subwrapper').prepend('<h2>Time Remaining: <span id="counter-number">60</span> Seconds</h2>');
+    $('#start').remove();
+
+
+    for (var i = 0; i < questions.length; i++) {
+      panel.append('<h2>' + questions[i].question + '</h2>');
+      for (var j = 0; j < questions[i].answers.length; j++) {
+        panel.append('<input type="radio" name="question' + '-' + i + '" value="' + questions[i].answers[j] + '">' + questions[i].answers[j]);
+      }
+    }
+
+    panel.append('<button id="done">Done</button>');
+  },
+    done: function() {
+
+
+    $.each($("input[name='question-0']:checked"), function() {
+      if ($(this).val() == questions[0].correctAnswer) {
+        game.correct++;
+      } else {
+        game.incorrect++;
+      }
+    });
+    $.each($("input[name='question-1']:checked"), function() {
+        if ($(this).val() == questions[1].correctAnswer) {
+        game.correct++;
+      } else {
+        game.incorrect++;
+      }
+    });
+    $.each($("input[name='question-2']:checked"), function() {
+      if ($(this).val() == questions[2].correctAnswer) {
+        game.correct++;
+      } else {
+        game.incorrect++;
+      }
+    });
+    $.each($("input[name='question-3']:checked"), function() {
+      if ($(this).val() == questions[3].correctAnswer) {
+        game.correct++;
+      } else {
+        game.incorrect++;
+      }
+    });
+    $.each($("input[name='question-4']:checked"), function() {
+      if ($(this).val() == questions[4].correctAnswer) {
+        game.correct++;
+      } else {
+        game.incorrect++;
+      }
+    });
+    $.each($("input[name='question-5']:checked"), function() {
+      if ($(this).val() == questions[5].correctAnswer) {
+        game.correct++;
+      } else {
+        game.incorrect++;
+      }
+    });
+    $.each($("input[name='question-6']:checked"), function() {
+      if ($(this).val() == questions[6].correctAnswer) {
+        game.correct++;
+      } else {
+        game.incorrect++;
+      }
+    });
+    $.each($("input[name='question-7']:checked"), function() {
+      if ($(this).val() == questions[7].correctAnswer) {
+        game.correct++;
+      } else {
+        game.incorrect++;
+      }
+    });
+
+    this.result();
+  },
+    result: function() {
+
+    clearInterval(timer);
+
+    $('#subwrapper h2').remove();
+    panel.html('<h2>All Done!</h2>');
+    panel.append('<h3>Correct Answers: ' + this.correct + '</h3>');
+    panel.append('<h3>Incorrect Answers: ' + this.incorrect + '</h3>');
+    panel.append('<h3>Unanswered: ' + (questions.length - (this.incorrect + this.correct)) + '</h3>');
+  }
+
+};
